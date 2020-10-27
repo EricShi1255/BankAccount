8cb8fde05606a5d340ae8a96d457f0b391bff324
EricShi1255 <eshi20@stuy.edu>
2020-10-26 22:34:12 -0400

    withdraw and deposit

--- BankAccount.java
+++ BankAccount.java
@@ -5,11 +5,11 @@ public class BankAccount {
     private String password;
    
     //constructor
-    public BankAccount(int accountID, String password) {
+    public BankAccount(int accountIDi, String passwordi) {
         //initialize
         balance = 0;
-        accountID = accountID;
-        password = password;
+        accountID = accountIDi;
+        password = passwordi;
     }
     /*standard accessor methods*/ 
     public double getBalance(){
@@ -20,6 +20,29 @@ public class BankAccount {
     } 
     public void setPassword(String newPass){
         password = newPass;
+    }
+    public boolean deposit(double amount){
+        //When the amount is non-negative increase the account balance by that much, and return true. 
+        //When the argument given is a negative you should leave the balance unchanged and return false. 
+        if (amount >= 0) {
+            balance += amount;
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    public boolean withdraw(double amount){
+        // This will not be successful if the balance is too low, or the amount is negative. In these cases do not change the balance. 
+        //When the amount is non-negative subtract it from the balance and return true if it was successful,false otherwise.
+        if (amount < 0 || amount > balance) {
+            return false;
+        }
+        else {
+            balance -= amount;
+            return true;
+        }
+        
     } 
 
 }
